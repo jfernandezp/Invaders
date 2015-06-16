0a953564b7c87a0c105cb84d659e596db9b72fcc
jfernandezpe <joseluisfp100@hotmail.com>
2015-06-17 01:26:08 +0200

    #5 Doble pantalla de splash, de 1,8 segundos cada una

--- src/launcher/InvadersLauncher.java
+++ src/launcher/InvadersLauncher.java
@@ -1,6 +1,7 @@
 package launcher;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 
 import javax.imageio.ImageIO;
 import javax.swing.JFrame;
@@ -11,43 +12,63 @@ import invaders.InvaderPanel;
 
 //Haciendose...
 /**
- * Invaders v1.1
+ * Invaders v0.3
  * @author jfernandezpe
- * @version 1.1 08/06/2015
+ * @version 0.3
+ * @since 2015/06/17
  *
  */
 public class InvadersLauncher {
-
-	private void loadImage(){
-	}
 	
-	public static void main(String[] args) {
+	JFrame frame;
+	JPanel panel;
+
+	InvadersLauncher(long tiempoInicial){
+
+        int ventanatx = 700;
+        int ventanaty = 500;
+		
 		String titulo = "Invaders";
 		Estado estado = new Estado();
 		
-		JFrame frame = new JFrame(titulo);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        int ventanatx = 700;
-        int ventanaty = 500;
-        JPanel panel = new LauncherPanel();
-        frame.add(panel, BorderLayout.CENTER);
-        frame.setSize(ventanatx, ventanaty);
-        frame.setVisible(true);
-        frame.setResizable(false);
-        frame.setTitle(titulo);
+		generateGUI(titulo,ventanatx,ventanaty);
+		
+		long tiempoFinal = System.currentTimeMillis();
+		
+		long tiempo = tiempoFinal - tiempoInicial;
+		
         try {
-			Thread.sleep(3000);
+			Thread.sleep((long) (1800 + tiempo));
+			((LauncherPanel) panel).nextSplash();
+			panel.repaint();
+			
+			Thread.sleep((long) (3600 + tiempo));
+			
 			frame.getContentPane().remove(panel);
 			panel = null;
 	        panel = new InvaderPanel(ventanatx, ventanaty,estado);
 			frame.add(panel, BorderLayout.CENTER);
 	        frame.paintAll(frame.getGraphics());
 		} catch (Exception e) {
-			// TODO Bloque catch generado automáticamente
-			e.printStackTrace();
-		}
-        
-		
+//			e.printStackTrace();
+		}	
+	}
+	
+	
+	private void generateGUI(String titulo, int ventanatx, int ventanaty){
+		frame = new JFrame(titulo);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        panel = new LauncherPanel();
+        frame.add(panel, BorderLayout.CENTER);
+        frame.setSize(ventanatx, ventanaty);
+        frame.setVisible(true);
+        frame.setResizable(false);
+        frame.setTitle(titulo);
+       }
+	
+	public static void main(String[] args) {
+		long tiempoInicial = System.currentTimeMillis();
+		new InvadersLauncher(tiempoInicial);			
 	}
 
 }
--- src/launcher/LauncherPanel.java
+++ src/launcher/LauncherPanel.java
@@ -1,21 +1,43 @@
 package launcher;
 
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
+
+import javax.swing.ImageIcon;
 import javax.swing.JPanel;
-import javax.swing.JLabel;
 
 public class LauncherPanel extends JPanel {
 
 	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private String[] splash;
+	private int cursor;
+	/**
 	 * Create the panel.
+	 * @param frame 
 	 */
 	public LauncherPanel() {
-		
-		JLabel lblNewLabel = new JLabel("jferandez presenta:");
-		add(lblNewLabel);
-		
-		JLabel label = new JLabel("Invaders");
-		add(label);
-
+		cursor = 0;
+		String[] tmpsplash = {"img/splashjfernandezpe.png","img/splashinvaders.png"};
+		splash = tmpsplash;		
 	}
+
+	 @Override
+    public void paintComponent(Graphics g) {
+		Graphics2D g2d = (Graphics2D) g;
+		Image image = new ImageIcon(splash[cursor]).getImage();
+		g2d.drawImage(image, 0, 0, this);
+	 }
+	 
+	 public void nextSplash(){
+		 if (cursor < (splash.length - 1)){
+			 cursor++;
+		 } else {
+			 cursor = 0;
+		 }
+	 }
 
 }
